b8e00afcf4e799bca305e4ab195151356a403d19
Ruslan Akbashev <ruslashev@gmail.com>
2017-09-15 22:55:25 +0300

    main: abstract away wav writing

--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,27 @@
 #include <cmath>
 #include <fstream>
+#include <array>
 #include <vector>
 
-template <typename T>
-void write_int(std::ostream &stream, T value) {
+typedef std::array<std::vector<uint16_t>, 2> samples_t;
+
+template <typename T> static void write_int(std::ostream &stream, T value) {
   for (unsigned size = sizeof(T); size; --size, value >>= 8)
     stream.put(static_cast<char>(value & 0xFF));
 }
 
-int main() {
-  std::ofstream f("out.wav", std::ios::binary);
+static void write_wav(const char *filename, int sample_rate
+    , samples_t samples) {
+  size_t num_samples = samples[0].size();
 
   // http://soundfile.sapp.org/doc/WaveFormat
   // RIFF chunk
   uint32_t riff_id = 0x46464952; // "RIFF"
-  uint32_t riff_size; // filled in later, 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
   uint32_t riff_format = 0x45564157; // "WAVE"
 
-  // fmt subchunk
+  // fmt subchunk (subchunk 1)
   uint32_t fmt_id = 0x20746d66; // "fmt "
-  uint32_t fmt_size = 16; // size of this 
+  uint32_t fmt_size = 16; // size of this subchunk
   uint16_t fmt_audio_format = 1; // no compression
   uint16_t fmt_num_channels = 2;
   uint32_t fmt_sample_rate = 44100;
@@ -28,26 +30,15 @@ int main() {
     * fmt_bits_per_sample / 8;
   uint16_t fmt_block_align = fmt_num_channels * fmt_bits_per_sample / 8;
 
-  // data subchunk
+  // data subchunk (subchunk 2)
   uint32_t data_id = 0x61746164; // "data"
-  uint32_t data_size; // num_samples * num_channels * bits_per_sample / 8
+  // num_samples * num_channels * bits_per_sample / 8
+  uint32_t data_size = num_samples * fmt_num_channels * fmt_bits_per_sample / 8;
 
-  double max_amplitude = 32768;
-  double hz = fmt_sample_rate;
-  double frequency = 261.626 /* C4 */, seconds = 2.5;
-  int num_samples = hz * seconds;  // total number of samples
-  std::vector<std::vector<uint16_t>> samples = { // 2 channels
-    std::vector<uint16_t>(num_samples), std::vector<uint16_t>(num_samples)
-  };
-  for (int n = 0; n < num_samples; n++) {
-    double amplitude = (double)n / num_samples * max_amplitude;
-    double value = sin((M_2_PI * n * frequency) / hz);
-    samples[0][n] = amplitude  * value;
-    samples[1][n] = (max_amplitude - amplitude) * value;
-  }
+  // belongs to riff chunk, 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
+  uint32_t riff_size = 4 + 8 + fmt_size + 8 + data_size;
 
-  data_size = num_samples * fmt_num_channels * fmt_bits_per_sample / 8;
-  riff_size = 4 + 8 + fmt_size + 8 + data_size;
+  std::ofstream f(filename, std::ios::binary);
 
   write_int(f, riff_id);
   write_int(f, riff_size);
@@ -62,9 +53,27 @@ int main() {
   write_int(f, fmt_bits_per_sample);
   write_int(f, data_id);
   write_int(f, data_size);
-  for (size_t i = 0; i < samples[0].size(); ++i) {
+  for (size_t i = 0; i < num_samples; ++i) {
     write_int(f, samples[0][i]);
     write_int(f, samples[1][i]);
   }
+}
+
+int main() {
+  double max_amplitude = 32760;
+  double sample_rate = 44100;
+  double frequency = 261.626 /* C4 */, seconds = 2.5;
+  int num_samples = sample_rate * seconds;
+  samples_t samples = {
+    std::vector<uint16_t>(num_samples), std::vector<uint16_t>(num_samples)
+  };
+  for (int n = 0; n < num_samples; n++) {
+    double amplitude = (double)n / num_samples * max_amplitude;
+    double value = sin((2 * M_PI * n * frequency) / sample_rate);
+    samples[0][n] = amplitude  * value;
+    samples[1][n] = (max_amplitude - amplitude) * value;
+  }
+
+  write_wav("out.wav", sample_rate, samples);
 }
 
