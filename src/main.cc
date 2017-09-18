91f816b73cefa8bfbe95fdcdd83086d0d76d7b39
Ruslan Akbashev <ruslashev@gmail.com>
2017-09-18 20:06:59 +0300

    Minor changes before adding parsing etc

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,24 @@
 #include <cmath>
 #include "wav_writer.hh"
 
-int main() {
-  double max_amplitude = 32760;
-  double sample_rate = 44100;
-  double frequency = 261.626 /* C4 */, seconds = 2.5;
-  int num_samples = sample_rate * seconds;
+samples_t create_2chan_samples(uint64_t num_samples) {
   samples_t samples = {
     std::vector<uint16_t>(num_samples), std::vector<uint16_t>(num_samples)
   };
-  for (int n = 0; n < num_samples; n++) {
-    double amplitude = (double)n / num_samples * max_amplitude;
-    double value = sin((2 * M_PI * n * frequency) / sample_rate);
-    samples[0][n] = amplitude  * value;
-    samples[1][n] = (max_amplitude - amplitude) * value;
+  return samples;
+}
+
+int main() {
+double max_amplitude = 32760;
+  double sample_rate = 44100;
+  double frequency = 261.626 /* C4 */, seconds = 2.5;
+  uint64_t num_samples = sample_rate * seconds;
+  samples_t samples = create_2chan_samples(num_samples);
+  double inv_sample_rate = 1. / (double)sample_rate;
+  for (uint64_t i = 0; i < num_samples; i++) {
+    double value = sin(2 * M_PI * i * frequency * inv_sample_rate);
+    samples[0][i] = max_amplitude * value;
+    samples[1][i] = max_amplitude * value;
   }
 
   write_wav("out.wav", sample_rate, samples);
