dfebeb7f6783e2f1ddf7034def1fc61dcaf30ad2
Ruslan Akbashev <ruslashev@gmail.com>
2017-09-21 13:54:24 +0300

    Move function definitions from lang.hh to lang.cc

--- src/lang.hh
+++ src/lang.hh
@@ -36,44 +36,9 @@ struct program_t {
 };
 
 term_t term_function(std::string name, std::vector<std::string> args
-    , term_t body) {
-  term_t t;
-  t.type = term_k::function;
-  t.function.name = new std::string(name);
-  t.function.args = new std::vector<std::string>(args);
-  t.function.body = new term_t(body);
-  return t;
-}
-
-term_t term_application(std::string name, std::vector<term_t> parameters) {
-  term_t t;
-  t.type = term_k::application;
-  t.application.name = new std::string(name);
-  t.application.parameters = new std::vector<term_t>(parameters);
-  return t;
-}
-
-term_t term_variable(std::string name) {
-  term_t t;
-  t.type = term_k::variable;
-  t.variable.name = new std::string(name);
-  return t;
-}
-
-term_t term_number(double value) {
-  term_t t;
-  t.type = term_k::number;
-  t.number.value = value;
-  return t;
-}
-
-std::string term_type_to_string(const term_t &term) {
-  switch (term.type) {
-    case term_k::function:    return "function";
-    case term_k::application: return "application";
-    case term_k::variable:    return "variable";
-    case term_k::number:      return "number";
-    default:                  return "unhandled";
-  }
-}
+    , term_t body);
+term_t term_application(std::string name, std::vector<term_t> parameters);
+term_t term_variable(std::string name);
+term_t term_number(double value);
+std::string term_type_to_string(const term_t &term);
 
